63c1ad2025d68526ff4d39da700733f75bd773bf
Julien Malka <julien.malka@me.com>
2022-09-21 06:41:35 +0200

    Refactored main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,15 +17,13 @@ pub struct Calendar {
     path: String,
 }
 
-#[tokio::main]
-async fn main() {
-    let path = "./data/calendars.json";
-    let data = fs::read_to_string(path).expect("Unable to read file");
-    let res: serde_json::Value = serde_json::from_str(&data).unwrap();
-    let cals = res["calendars"].clone();
-    let obj: Map<String, Value> = cals.as_object().unwrap().clone();
+fn parse_calendar_json(path: &str) -> HashMap<String, Calendar> {
+    let json_data = fs::read_to_string(path).expect("Unable to read calendars.json file");
+    let cals: serde_json::Value = serde_json::from_str(&json_data).unwrap();
+    let cals = cals["calendars"];
+    let cals: Map<String, Value> = cals.as_object().unwrap().clone();
     let mut calendars = HashMap::new();
-    for (k, v) in obj {
+    for (k, v) in cals {
         let split = k.clone();
         let splitted = split.split("/");
         let vec: Vec<&str> = splitted.collect();
@@ -38,7 +36,14 @@ async fn main() {
             },
         );
     }
-    dbg!(calendars.clone());
+    calendars
+}
+
+#[tokio::main]
+async fn main() {
+    //TODO Parse command line arguments to get this path
+    let path = "./data/calendars.json";
+    let calendars = parse_calendar_json(path);
     pretty_env_logger::init();
     let routes = filters::api(calendars);
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
