a20611d2e85718e5b863b70d31bad7682b5bd88c
Julien Malka <julien.malka@me.com>
2022-09-23 10:42:16 +0200

    Parse fields from propfind request

--- src/propfind.rs
+++ src/propfind.rs
@@ -1,5 +1,17 @@
-pub fn parse_propfind(request: &str) -> exile::Element {
+use exile::Node::Element;
+pub fn parse_propfind(request: &str) -> Vec<String> {
     let tree = exile::parse(request).unwrap();
-    let root = tree.root().to_owned();
-    return root;
+    let root = tree.root();
+    let prop: &exile::Node = root.first_node().unwrap();
+    let mut result: Vec<String> = Vec::new();
+
+    let fields = match prop {
+        Element(e) => e.children(),
+        _ => panic!("Propfind request is not correctly formed"),
+    };
+
+    for field in fields {
+        result.push(field.name().to_string());
+    }
+    return result;
 }
